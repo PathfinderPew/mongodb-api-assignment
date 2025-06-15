7015c2ee20e177e8f008e14d45ed1ba29df4b79f
Brandon P <pew16002@byui.edu>
2025-06-14 22:43:19 -0600

    Manually registered GET /contacts in Swagger

--- swagger.js
+++ swagger.js
@@ -1,24 +1,49 @@
-const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Contacts API',
-      version: '1.0.0',
-      description: 'API documentation for the Contacts project',
-    },
-    servers: [
-      {
-        url: 'https://mongodb-api-assignment.onrender.com', // deployed URL
-      }
-    ],
+const swaggerSpec = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Contacts API',
+    version: '1.0.0',
+    description: 'API documentation for the Contacts project',
   },
-  apis: ['./routes/*.js'], // route files for auto doc generation
+  servers: [
+    {
+      url: 'https://mongodb-api-assignment.onrender.com',
+    },
+  ],
+  paths: {
+    '/contacts': {
+      get: {
+        tags: ['Contacts'],
+        summary: 'Get all contacts',
+        responses: {
+          200: {
+            description: 'A list of contacts',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      _id: { type: 'string' },
+                      firstName: { type: 'string' },
+                      lastName: { type: 'string' },
+                      email: { type: 'string' },
+                      favoriteColor: { type: 'string' },
+                      birthday: { type: 'string' }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 };
-
-const swaggerSpec = swaggerJSDoc(options);
 
 function setupSwagger(app) {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
